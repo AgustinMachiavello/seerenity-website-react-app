7fffcf1f1faee40802d74e3e77048b4a8e43ee84
AgustinMachiavello <agmachiavello@gmail.com>
2023-10-12 23:10:18 +0200

    list first project

--- client/src/containers/ProjectsListPage/index.js
+++ client/src/containers/ProjectsListPage/index.js
@@ -1,17 +1,64 @@
 import React, { useState, useEffect } from "react";
-import { Page } from "grommet";
+import { Box, Card, CardHeader, Page } from "grommet";
 
 import { useEth } from "@contexts/EthContext";
 
+import ProjectContractJson from "../../contracts/ProjectContract.json";
+import { CONTRACT_ADDRESSES } from "../../contract-constants";
+
 const ProjectsListPage = () => {
+  // Hooks
+  const { state } = useEth();
+
   // States
-  const eth = useEth();
+  const [projectContract, setProjectContract] = useState();
+  const [projects, setProjects] = useState([]);
 
-  console.warn("eth", eth);
+  // Data
+  const userAccount = state.accounts?.[0];
+  console.warn("projectContract", projectContract);
+  console.warn("projects", projects);
+  console.warn("state", state);
+  console.warn("useraccount", userAccount);
 
-  useEffect(() => {}, []);
+  const fetchProjects = async () => {
+    await projectContract.methods
+      .getProjects()
+      .call()
+      .then((pList) => {
+        setProjects(pList);
+      });
+  };
 
-  return <Page>Pl page</Page>;
+  // Effects
+  useEffect(() => {
+    if (state.web3?.eth) {
+      const projectContract = new state.web3.eth.Contract(
+        ProjectContractJson.abi,
+        CONTRACT_ADDRESSES.project
+      );
+      setProjectContract(projectContract);
+    }
+  }, [state]);
+
+  useEffect(() => {
+    if (projectContract?.methods) {
+      fetchProjects();
+    }
+  }, [projectContract]);
+
+  return (
+    <Page>
+      Pl page Count: {projects.length}
+      <Box pad="large" gap="medium">
+        {projects.map((project) => (
+          <Card>
+            <CardHeader>{project.projectName}</CardHeader>
+          </Card>
+        ))}
+      </Box>
+    </Page>
+  );
 };
 
 export default ProjectsListPage;
